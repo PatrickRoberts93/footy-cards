4d4dfb0bb7a2143d0dbcd9b750cb86db0a56a5b7
Patrick Roberts <patrickroberts93@gmail.com>
2018-01-21 15:16:54 +1100

    basic burger menu styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isCollapsed: boolean;
+
+  constructor() {
+    this.isCollapsed = true;
+  }
 
   ngOnInit() {
   }
