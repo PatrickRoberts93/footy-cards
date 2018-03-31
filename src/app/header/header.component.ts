3985f0f9bfaa163b76ceb220253e18b1c79a0fed
Patrick Roberts <patrickroberts93@gmail.com>
2018-03-31 18:24:51 +1100

    Added sticky header elements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+  // STICKY HEADER ELEMENTS
+  @HostListener('window:scroll', [ ])
+    onWindowScroll(){
+      let scrollPos = document.scrollingElement.scrollTop + 74,
+          header = document.getElementById("header"),
+          headerText = document.getElementById("header-text"),
+          filterBar = document.getElementById("filter-bar");
+
+      // PAGE TITLE
+      if (headerText.offsetTop < scrollPos) {
+        headerText.classList.add('fixed');
+      } else {
+       headerText.classList.remove('fixed');
+      }
+
+      // FILTER BAR
+      if ((header.offsetHeight - filterBar.offsetHeight) < scrollPos) {
+        filterBar.classList.add('fixed');
+      } else {
+        filterBar.classList.remove('fixed');
+      }
+    }
 
   ngOnInit() {
   }
