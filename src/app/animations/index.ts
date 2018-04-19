9b13675f1c79fd618e5db1d2453b2f6b73a91dbb
Patrick Roberts <patrickroberts93@gmail.com>
2018-04-19 20:03:42 +1000

    added optional parameter to router transition query

--- src/app/animations/index.ts
+++ src/app/animations/index.ts
@@ -3,15 +3,15 @@ import { trigger, state, animate, transition, style, query, stagger, keyframes, 
 export const routerTransition =
      trigger('routerTransition', [
         transition('* <=> *', [    
-          query(':enter, :leave', style({ position: 'absolute', width:'100%', top: '0', left: '0' })),
-          group([ 
+          query(':enter, :leave', style({ position: 'absolute', width:'100%', top: '0', left: '0' }), {optional: true}),
+          group([
             query(':enter', [
               style({ transform: 'translateX(200%)' }),
               animate('300ms ease-in-out', style({ transform: 'translateX(0%)' }))
-            ]),
+            ], {optional: true}),
             query(':leave', [
               style({ transform: 'translateX(0%)' }),
-              animate('300ms ease-in-out', style({ transform: 'translateX(-300%)' }))]),
+              animate('300ms ease-in-out', style({ transform: 'translateX(-300%)' }))], {optional: true}),
           ])
         ])
       ]);
